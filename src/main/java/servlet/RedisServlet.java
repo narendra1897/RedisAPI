0fbb512b950d4511251825d16c9c1a13e2b76708
Shailendra Chaudhary <Shailendra Chaudhary>
2017-01-30 10:25:45 -0800

    lsit implemented

--- src/main/java/servlet/RedisServlet.java
+++ src/main/java/servlet/RedisServlet.java
@@ -14,29 +14,6 @@ import  java.util.List;
  */
 public class RedisServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response){
-        response.setContentType("text/html");
-        String country="country";
-        System.out.println("hello");
-        //Map parameter = request.getParameterMap();
-        //System.out.println(parameter);
-        Jedis jedis = ConnectRedis.conn();
-        /*System.out.println("the server ping " + jedis.ping());
-        jedis.set("name","narendra");
-        jedis.set(country,"idnia");
-        System.out.println("the name is " + jedis.get("name"));
-        System.out.println("your country is : " + jedis.get(country));
-        jedis.lpush("students","shailendra");
-        jedis.lpush("students","john");
-        List<String> mylist = jedis.lrange("students",0,-1);
-        for(String elements : mylist){
-            System.out.print(elements + "  ");
-        }*/
-        //System.out.println("the query is " + request.getQueryString());
-        System.out.println("see below");
-        String input = request.getQueryString();
-        String[] out = input.split("-");
-        for(String elem : out){
-            System.out.println(elem + "  ");
-        }
+
     }
 }
